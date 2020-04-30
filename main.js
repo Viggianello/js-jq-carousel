63874e5fa3150a2885797c507c4f1feb3f5586cf
Antonello Viggiano <antonelloviggiano1990@hotmail.it>
2020-04-30 16:04:21 +0200

    inserito pallino prev

--- main.js
+++ main.js
@@ -33,18 +33,28 @@ $('.next').click(function() {
 $('.prev').click(function() {
     // recupero l'img che ha la classe active in questo momento
     var img_corrente = $('img.active');
+    // recupero il pallino che ha la classe active in questo momento
+    var pallino_corrente = $('.fa-circle.active');
     // tolgo la classe active dall'img corrente
     img_corrente.removeClass('active');
+    // tolgo la classe active al pallino corrente
+    pallino_corrente.removeClass('active');
     // recupero l'immagine precedente
     var img_precedente = img_corrente.prev('img');
+    // recupero il pallino precedente
+    var pallino_precedente =pallino_corrente.prev('.fa-circle');
     // verifico che esista un img precedente
     if (img_precedente.length !=0) {
         // c'è una img precedente
         // metto la classe active all'immagine img_precedente
         img_precedente.addClass('active');
+        // metto la classe active al pallino precedente
+        pallino_precedente.addClass('active');
     } else {
         // non c'è una img precedente => riparto dalla fine
         img_precedente = $('img:last-of-type');
         img_precedente.addClass('active');
+        // non c'è un pallino precedente => riparto dall'ultimo
+        $('.fa-circle:last-child').addClass('active');
     }
 });
