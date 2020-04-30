13319ca3cedf8abdcbd50d3a806620e15061f1d9
Antonello Viggiano <antonelloviggiano1990@hotmail.it>
2020-04-30 15:59:32 +0200

    inserito pallino next

--- main.js
+++ main.js
@@ -2,19 +2,29 @@
 $('.next').click(function() {
     // recupero l'img che ha la classe active in questo momento
     var img_corrente = $('img.active');
+    // recuro il pallino che ha la classe active in questo momento
+    var pallino_corrente = $('.fa-circle.active');
     // tolgo la classe active dall'img corrente
     img_corrente.removeClass('active');
+    // tolgo la classe active al pallino corrente
+    pallino_corrente.removeClass('active');
     // recupero l'immagine successiva
     var img_successiva = img_corrente.next('img');
+    // recupero il pallino successiva
+    var pallino_successivo =pallino_corrente.next('.fa-circle');
     // verifico che esista un img successivo
     if (img_successiva.length !=0) {
         // c'è una img successiva
         // metto la classe active all'immagine img_successiva
         img_successiva.addClass('active');
+        // metto la classe active al pallino successivo
+        pallino_successivo.addClass('active');
     } else {
         // non c'è una img succesiva => riparto dall'inizio
         img_successiva = $('img:first-child');
         img_successiva.addClass('active');
+        // non c'è un pallino succesivo => riparto dal primo
+        $('.fa-circle:first-child').addClass('active');
     }
 });
 
@@ -34,7 +44,7 @@ $('.prev').click(function() {
         img_precedente.addClass('active');
     } else {
         // non c'è una img precedente => riparto dalla fine
-        img_precedente = $('img:last-child');
+        img_precedente = $('img:last-of-type');
         img_precedente.addClass('active');
     }
 });
